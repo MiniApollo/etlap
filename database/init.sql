21823ff1af2f5b96209ef6e12912e7d8697ce8a1
MiniApollo <surmannmark@gmail.com>
2024-10-20 13:19:52 +0200

    Database: Add example data, logic, fix typos

--- database/init.sql
+++ database/init.sql
@@ -1,23 +1,29 @@
+DROP DATABASE IF EXISTS etlap;
 CREATE DATABASE etlap;
 USE etlap;
-CREATE TABLE Vasarlo (
-	   RendelesID int NOT NULL,
+CREATE TABLE Vasarlok (
+	   VasarloID int NOT NULL,
 	   Nev varchar(128) NOT NULL,
 	   Email varchar(128) NOT NULL,
-	   Telefonszam int,
-	   PRIMARY KEY(RendelesID)
+	   Telefonszam int NOT NULL,
+	   PRIMARY KEY(VasarloID)
 );
 CREATE TABLE Etelek (
 	   EtelID int NOT NULL,
 	   Nev varchar(128) NOT NULL,
 	   Leiras varchar(1024),
 	   Kep varchar(128),
-	   Ar DOUBLE NOT NULL,
+	   Ar DECIMAL NOT NULL,
 	   PRIMARY KEY(EtelID)
 );
 CREATE TABLE Rendelesek (
-	   RendelesID int NOT NULL ,
+	   VasarloID int NOT NULL,
 	   EtelID int NOT NULL,
-	   FOREIGN KEY (RendelesID) REFERENCES Vasarlo(RendelesID),
+	   FOREIGN KEY (VasarloID) REFERENCES Vasarlok(VasarloID),
 	   FOREIGN KEY (EtelID) REFERENCES Etelek(EtelID)
 );
+
+INSERT INTO Vasarlok (VasarloID, Nev, Email, Telefonszam) VALUES
+(1, "Bob Nagy", "bob@example.com", 36201234),
+(2, "Jakab", "jakab@example.com", 12345),
+(3, "Cecilia", "cecilia@example.com", 54321);
