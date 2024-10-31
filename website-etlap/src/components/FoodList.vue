72cadc764b92f6e07baa0857c37f54849866f4d7
MiniApollo <surmannmark@gmail.com>
2024-10-31 17:18:22 +0100

    Frontend: Implement food list functionality

--- website-etlap/src/components/FoodList.vue
+++ website-etlap/src/components/FoodList.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const foods: any = ref([]);
+
+async function getAllFood() {
+    fetch("http://localhost:8080/food")
+        .then(res => res.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                let food = { EtelID: data[i].EtelID, Nev: data[i].Nev, Leiras: data[i].Leiras, Kep: data[i].Kep, Ar: data[i].Ar };
+                foods.value.push(food);
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+onMounted(() => {
+    getAllFood();
+});
 
 </script>
 
 <template>
     <h1 class="text-3xl">Ételek Listája</h1>
+    <ul>
+        <li v-for="food in foods">
+            <h3 class="text-2xl">{{ food.Nev }}</h3>
+            <p>{{ food.Leiras }}</p>
+            {{ food.Kep }}
+            <p>{{ food.Ar }} Ft</p>
+        </li>
+    </ul>
 
 </template>
