301c38880dfb73456405cec8f8565b4baf6bd42f
MiniApollo <surmannmark@gmail.com>
2024-11-17 10:54:44 +0100

    Database: Auto increment all IDs

--- database/Etel_adatok.sql
+++ database/Etel_adatok.sql
@@ -1,9 +1,9 @@
-INSERT INTO Etelek (EtelID , Nev, Leiras, Kep , Ar) VALUES
-(1, "Húsleves", "A húsleves állati eredetű húsból, csontból és zöldségekből készített leves.", "", 1650),
-(2, "Paprikás krumpli", "A paprikás krumpli magyar egytálétel, melynek fő összetevői a burgonya, az őrölt pirospaprika, a kolbász vagy virsli és a vöröshagyma.", "", 2100),
-(3, "Rántotthús", "Rántva finomabb ;)", "", 1000),
-(4, "Palacsinta", "Az édességek édessége.", "", 850),
-(5, "Lángos", "A lángos a magyar konyha hagyományos, lágy kelt tésztából készülő, sült lepénye.", "", 1930),
-(6, "Brassói aprópecsenye", "A brassói aprópecsenye egy sertéshúsból készített főfogás.", "", 3100),
-(7, "Lecsó", "Nem a mese >:(", "", 2500),
-(8, "Gulyás", "A „magyar gulyásleves” 2017 óta hungarikum.", "", 1500);
+INSERT INTO Etelek (Nev, Leiras, Kep , Ar) VALUES
+("Húsleves", "A húsleves állati eredetű húsból, csontból és zöldségekből készített leves.", "", 1650),
+("Paprikás krumpli", "A paprikás krumpli magyar egytálétel, melynek fő összetevői a burgonya, az őrölt pirospaprika, a kolbász vagy virsli és a vöröshagyma.", "", 2100),
+("Rántotthús", "Rántva finomabb ;)", "", 1000),
+("Palacsinta", "Az édességek édessége.", "", 850),
+("Lángos", "A lángos a magyar konyha hagyományos, lágy kelt tésztából készülő, sült lepénye.", "", 1930),
+("Brassói aprópecsenye", "A brassói aprópecsenye egy sertéshúsból készített főfogás.", "", 3100),
+("Lecsó", "Nem a mese >:(", "", 2500),
+("Gulyás", "A „magyar gulyásleves” 2017 óta hungarikum.", "", 1500);
--- database/init.sql
+++ database/init.sql
@@ -2,14 +2,14 @@ DROP DATABASE IF EXISTS etlap;
 CREATE DATABASE etlap;
 USE etlap;
 CREATE TABLE Vasarlok (
-	   VasarloID int NOT NULL,
+	   VasarloID int NOT NULL AUTO_INCREMENT,
 	   Nev varchar(128) NOT NULL,
 	   Email varchar(128) NOT NULL,
 	   Telefonszam int NOT NULL,
 	   PRIMARY KEY(VasarloID)
 );
 CREATE TABLE Etelek (
-	   EtelID int NOT NULL,
+	   EtelID int NOT NULL AUTO_INCREMENT,
 	   Nev varchar(128) NOT NULL,
 	   Leiras varchar(1024),
 	   Kep varchar(128),
@@ -23,7 +23,7 @@ CREATE TABLE Rendelesek (
 	   FOREIGN KEY (EtelID) REFERENCES Etelek(EtelID)
 );
 
-INSERT INTO Vasarlok (VasarloID, Nev, Email, Telefonszam) VALUES
-(1, "Bob Nagy", "bob@example.com", 36201234),
-(2, "Jakab", "jakab@example.com", 12345),
-(3, "Cecilia", "cecilia@example.com", 54321);
+INSERT INTO Vasarlok (Nev, Email, Telefonszam) VALUES
+("Bob Nagy", "bob@example.com", 36201234),
+("Jakab", "jakab@example.com", 12345),
+("Cecilia", "cecilia@example.com", 54321);
