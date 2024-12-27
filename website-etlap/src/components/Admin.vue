0fd66f624ac44fb841afc540ee23269a993840c4
MiniApollo <surmannmark@gmail.com>
2024-12-27 21:19:13 +0100

    Frontend: Handle empty customers array in admin

--- website-etlap/src/components/Admin.vue
+++ website-etlap/src/components/Admin.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
-const password: any = ref("");
+const password = ref("");
 const isLoggedIn = ref();
 const responseMessage = ref("");
 
@@ -9,12 +9,23 @@ const orders: any = ref([]);
 const customers: any = ref([]);
 const foodsByCustomer: any = ref([]);
 
+const statusMessage = ref("");
+const statusError = ref(false);
+const showCurrentOrders = ref(true);
+
 async function getData() {
     orders.value = await getWithToken("order");
     customers.value = await getWithToken("order/customer");
+    if (!Array.isArray(customers.value)) {
+        statusMessage.value = "Nincs új rendelés";
+        statusError.value = true;
+        return
+    }
+
     for (let i = 0; i < customers.value.length; i++) {
         foodsByCustomer.value.push(await getWithToken("order/food/" + customers.value[i].VasarloID));
     }
+    console.log(customers.value);
     console.log(foodsByCustomer.value);
 }
 
@@ -99,29 +110,35 @@ onMounted(() => {
             <h3>{{ responseMessage }}</h3>
         </form>
         <div v-else-if="isLoggedIn">
-            <h3>{{ responseMessage }}</h3>
             <button @click="signOut">Kijelentkezés</button>
+
             <h3>Rendelések:</h3>
-            <ul>
-                <li v-for="(customer, index) in customers" class="m-1 p-1 border border-black">
-                    <ul>
-                        <li class="inline m-1">{{ customer.Nev }}</li>
-                        <li class="inline m-1">{{ customer.VasarloID }}</li>
-                        <li class="inline m-1">{{ customer.Email }}</li>
-                        <li class="inline m-1">{{ customer.Telefonszam }}</li>
-                        <li class="inline m-1">{{ customer.LeadasiIdo }}</li>
-                        <button @click="orderDone(customer)">Kész</button>
-                    </ul>
-                    <ul>
-                        <li v-for="food in foodsByCustomer[index]">
-                            <ul>
-                                <li class="inline m-1">{{ food.Nev }}</li>
-                                <li class="inline m-1">{{ food.Ar }}</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <h3 v-if="statusError">{{ statusMessage }}</h3>
+
+            <div v-else-if="!statusError">
+                <ul>
+                    <button @click="showCurrentOrders = !showCurrentOrders">Kész Rendelések mutatása</button>
+
+                    <li v-for="(customer, index) in customers" class="m-1 p-1 border border-black">
+                        <ul>
+                            <li class="inline m-1">{{ customer.Nev }}</li>
+                            <li class="inline m-1">{{ customer.VasarloID }}</li>
+                            <li class="inline m-1">{{ customer.Email }}</li>
+                            <li class="inline m-1">{{ customer.Telefonszam }}</li>
+                            <li class="inline m-1">{{ customer.LeadasiIdo }}</li>
+                            <button @click="orderDone(customer)">Kész</button>
+                        </ul>
+                        <ul>
+                            <li v-for="food in foodsByCustomer[index]">
+                                <ul>
+                                    <li class="inline m-1">{{ food.Nev }}</li>
+                                    <li class="inline m-1">{{ food.Ar }}</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
