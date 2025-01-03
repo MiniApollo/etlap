b4afeebb0b9aadc2e2c301e79aabfa765c21ebd6
MiniApollo <surmannmark@gmail.com>
2025-01-03 13:00:12 +0100

    Frontend: Add child component functionality, Refactor admin page

--- website-etlap/src/components/Admin.vue
+++ website-etlap/src/components/Admin.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
+import FoodEdit from './admin/foodEdit.vue';
+import CustomerData from './admin/customerData.vue';
 
 const password = ref("");
 const isLoggedIn = ref(false);
@@ -37,8 +39,8 @@ async function getData() {
     for (let i = 0; i < customers.value.length; i++) {
         foodsByCustomer.value.push(await getWithToken("order/food/" + customers.value[i].VasarloID));
     }
-    console.log(customers.value);
-    console.log(foodsByCustomer.value);
+    //console.log(customers.value);
+    //console.log(foodsByCustomer.value);
 }
 
 async function sendPassword() {
@@ -87,8 +89,7 @@ async function getWithToken(url: String) {
     return json;
 }
 
-async function orderDone(customer: any, isDone :boolean) {
-    console.log(isDone);
+async function orderDone(customer: any, isDone: boolean) {
 
     await fetch("http://localhost:8080/customer/" + customer.VasarloID, {
         method: "PATCH",
@@ -98,6 +99,24 @@ async function orderDone(customer: any, isDone :boolean) {
         },
         body: JSON.stringify({
             Elkeszult: isDone,
+        })
+    })
+    getData();
+}
+
+async function postNewFood(food: any, requestType: string = "POST", etelID: String = "") {
+
+    await fetch("http://localhost:8080/food/" + etelID, {
+        method: requestType,
+        headers: {
+            'Authorization': 'Bearer ' + btoa(sessionStorage.getItem("adminToken") || "{}"),
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify({
+            Nev: food.Nev,
+            Leiras: food.Leiras,
+            Kep: food.Kep,
+            Ar: food.Ar,
         })
     })
     getData();
@@ -132,63 +151,29 @@ onMounted(() => {
             <br>
             <button @click="isShowOrder = !isShowOrder; getData()">
                 <p v-if="!isShowOrder">Rendelések megjelenítése</p>
-                <p v-if="isShowOrder">Ételek szerkesztésének megjelenítése</p>
+                <p v-else-if="isShowOrder">Ételek szerkesztésének megjelenítése</p>
             </button>
 
             <div v-if="isShowOrder">
-
                 <button @click="showCurrentOrders = !showCurrentOrders; getData()">
                     <p v-if="showCurrentOrders">Korábbi rendelések megjelenítése</p>
-                    <p v-if="!showCurrentOrders">Jelenlegi rendelések megjelenítése</p>
+                    <p v-else-if="!showCurrentOrders">Jelenlegi rendelések megjelenítése</p>
                 </button>
 
                 <h3 v-if="showCurrentOrders">Jelenlegi Rendelések:</h3>
-                <h3 v-if="!showCurrentOrders">Korrábbi Rendelések:</h3>
+                <h3 v-else-if="!showCurrentOrders">Korrábbi Rendelések:</h3>
                 <ul>
                     <h3 v-if="statusError && showCurrentOrders">{{ statusMessage }}</h3>
-
                     <li v-if="Array.isArray(customers)" v-for="(customer, index) in customers"
                         class="m-1 p-1 border border-black">
-                        <ul>
-                            <li class="inline m-1">{{ customer.Nev }}</li>
-                            <li class="inline m-1">{{ customer.VasarloID }}</li>
-                            <li class="inline m-1">{{ customer.Email }}</li>
-                            <li class="inline m-1">{{ customer.Telefonszam }}</li>
-                            <li class="inline m-1">{{ customer.LeadasiIdo }}</li>
-                            <button @click="orderDone(customer, showCurrentOrders)">
-                                <p v-if="showCurrentOrders">Kész</p>
-                                <p v-if="!showCurrentOrders">Visszavon</p>
-                            </button>
-                        </ul>
-                        <ul>
-                            <li v-for="food in foodsByCustomer[index]">
-                                <ul>
-                                    <li class="inline m-1">{{ food.Nev }}</li>
-                                    <li class="inline m-1">{{ food.Ar }}</li>
-                                </ul>
-                            </li>
-                        </ul>
+                        <CustomerData @orderComplete="orderDone" :customer="customer"
+                            :foods-by-customer="foodsByCustomer" :show-current-orders="showCurrentOrders" :index="index" />
                     </li>
                 </ul>
             </div>
 
-            <div v-if="!isShowOrder">
-                <button>Új Étel hozzáadása</button>
-                <h1 class="text-3xl">Ételek Listája</h1>
-                <h2 v-if="foods === undefined || foods.length == 0">
-                    Hiba történt: <br>
-                    {{ statusMessage }}
-                </h2>
-                <ul v-else>
-                    <li v-for="food in foods" class="m-2 p-4 h-96 rounded-md border-black border-2">
-                        <h3 class="text-2xl">{{ food.Nev }}</h3>
-                        <p>{{ food.Leiras }}</p>
-                        <img :src="food.Kep" :alt="food.Nev + ' image'">
-                        <p>{{ food.Ar }} Ft</p>
-                        <button class="border-2 border-black">Módosítás</button>
-                        <button class="border-2 border-black">Törlés</button>
-                    </li>
-                </ul>
+            <div v-else-if="!isShowOrder">
+                <FoodEdit @newFoodSubmit="postNewFood" :foods="foods" :status-message="statusMessage" />
             </div>
 
         </div>
