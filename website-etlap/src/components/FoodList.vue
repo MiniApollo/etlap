08494417cfa2ae4402b0d8e16a030e95c59e5e34
MiniApollo <surmannmark@gmail.com>
2025-01-17 15:08:13 +0100

    Frontend: Add Display food image, more style to FoodList

--- website-etlap/src/components/FoodList.vue
+++ website-etlap/src/components/FoodList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { KeepAlive, onMounted, ref } from 'vue';
 
 const foods: any = ref([]);
 const error: any = ref();
@@ -9,7 +9,7 @@ async function getAllFood() {
         .then(res => res.json())
         .then(data => {
             for (let i = 0; i < data.length; i++) {
-                let food = { EtelID: data[i].EtelID, Nev: data[i].Nev, Leiras: data[i].Leiras, Kep: data[i].Kep, Ar: data[i].Ar };
+                let food: any = { EtelID: data[i].EtelID, Nev: data[i].Nev, Leiras: data[i].Leiras, KepPath: data[i].KepPath, Ar: data[i].Ar };
                 foods.value.push(food);
             }
         })
@@ -22,26 +22,28 @@ async function getAllFood() {
 onMounted(() => {
     getAllFood();
 });
-
 </script>
 
 <template>
     <!-- Fix warnings: 
      https://stackoverflow.com/questions/68803137/vue-3-passing-array-warning-extraneous-non-props-attributes-were-passed-to-comp-->
-    <div class="flex justify-evenly items-center flex-col ">
-        <h1 class="text-3xl">Ételek Listája</h1>
+    <div class="m-auto w-1/2">
+        <h1 class="px-5 text-3xl">Ételek Listája</h1>
         <h2 v-if="foods === undefined || foods.length == 0">
             Hiba történt: <br>
             {{ error }}
         </h2>
         <ul v-else>
             <!-- :style="{ 'background-image': 'url(' + food.Kep + ')'}" -->
-            <li v-for="food in foods" class="m-2 p-4 h-96 rounded-md border-black border-2">
-                <h3 class="text-2xl">{{ food.Nev }}</h3>
-                <p>{{ food.Leiras }}</p>
-                <img :src="food.Kep" :alt="food.Nev + ' image'">
-                <p>{{ food.Ar }} Ft</p>
-                <button class="border-2 border-black" @click="$emit('kosarhoz', food)"> Kosárhoz</button>
+            <li v-for="food in foods" class="m-6 h-96 rounded-md border-black border-2 bg-cover bg-no-repeat bg-center"
+                :style="{ 'background-image': 'url(' + 'http://localhost:8080/assets/' + food.KepPath + ')'}">
+                <!-- <img :src="'http://localhost:8080/assets/' + food.KepPath" :alt="food.Nev + ' kép'"> -->
+                <div class="bg-white bg-opacity-90 inline-block rounded-md p-2 max-w-[70%]">
+                    <h3 class="text-2xl">{{ food.Nev }}</h3>
+                    <p>{{ food.Leiras }}</p>
+                    <p>{{ food.Ar }} Ft</p>
+                    <button class="border-2 border-black" @click="$emit('kosarhoz', food)"> Kosárhoz</button>
+                </div>
             </li>
         </ul>
     </div>
