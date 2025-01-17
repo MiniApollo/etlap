acb210ff27fe240350f37e09e3edd3eb4b54f5e7
MiniApollo <surmannmark@gmail.com>
2025-01-17 15:07:35 +0100

    Database: Add example image path to insert into data

--- database/Etelek.sql
+++ database/Etelek.sql
@@ -1,8 +1,8 @@
 INSERT INTO Etelek (Nev, Leiras, Kep , Ar) VALUES
-("Húsleves", "A húsleves állati eredetű húsból, csontból és zöldségekből készített leves.", "", 1650),
+("Húsleves", "A húsleves állati eredetű húsból, csontból és zöldségekből készített leves.", "pancake.jpg", 1650),
 ("Paprikás krumpli", "A paprikás krumpli magyar egytálétel, melynek fő összetevői a burgonya, az őrölt pirospaprika, a kolbász vagy virsli és a vöröshagyma.", "", 2100),
 ("Rántotthús", "Rántva finomabb ;)", "", 1000),
-("Palacsinta", "Az édességek édessége.", "", 850),
+("Palacsinta", "Az édességek édessége.", "pancake.jpg", 850),
 ("Lángos", "A lángos a magyar konyha hagyományos, lágy kelt tésztából készülő, sült lepénye.", "", 1930),
 ("Brassói aprópecsenye", "A brassói aprópecsenye egy sertéshúsból készített főfogás.", "", 3100),
 ("Lecsó", "Nem a mese >:(", "", 2500),
