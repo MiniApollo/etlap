c9e7a7c5d21a5c8f73eaf0ecd8ead7b534bf7a35
MiniApollo <surmannmark@gmail.com>
2025-01-17 15:06:46 +0100

    Backend: Rename Kep to KepPath

--- internal/restapi/restapi.go
+++ internal/restapi/restapi.go
@@ -38,11 +38,11 @@ type order struct {
 }
 
 type food struct {
-	EtelID int     `json:"EtelID"`
-	Nev    string  `json:"Nev" binding:"required"`
-	Leiras string  `json:"Leiras" `
-	Kep    string  `json:"Kep" `
-	Ar     float64 `json:"Ar" binding:"required"`
+	EtelID  int     `json:"EtelID"`
+	Nev     string  `json:"Nev" binding:"required"`
+	Leiras  string  `json:"Leiras" `
+	KepPath string  `json:"KepPath" `
+	Ar      float64 `json:"Ar" binding:"required"`
 }
 
 // Capital Letters mean public
@@ -57,7 +57,7 @@ func GetAllFood(c *gin.Context) {
 	defer rows.Close()
 	for rows.Next() {
 		var food food
-		if err := rows.Scan(&food.EtelID, &food.Nev, &food.Leiras, &food.Kep, &food.Ar); err != nil {
+		if err := rows.Scan(&food.EtelID, &food.Nev, &food.Leiras, &food.KepPath, &food.Ar); err != nil {
 			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 			return
 		}
@@ -78,7 +78,7 @@ func GetAllFood(c *gin.Context) {
 
 func GetFood(c *gin.Context) {
 	var food food
-	err := Db.QueryRow("SELECT * FROM Etelek WHERE EtelID = ?", c.Param("id")).Scan(&food.EtelID, &food.Nev, &food.Leiras, &food.Kep, &food.Ar)
+	err := Db.QueryRow("SELECT * FROM Etelek WHERE EtelID = ?", c.Param("id")).Scan(&food.EtelID, &food.Nev, &food.Leiras, &food.KepPath, &food.Ar)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Not found"})
 		return
@@ -103,7 +103,7 @@ func GetAllFoodByCustomer(c *gin.Context) {
 			return
 		}
 		var food food
-		err := Db.QueryRow("SELECT * FROM Etelek WHERE EtelID = ?", order.EtelID).Scan(&food.EtelID, &food.Nev, &food.Leiras, &food.Kep, &food.Ar)
+		err := Db.QueryRow("SELECT * FROM Etelek WHERE EtelID = ?", order.EtelID).Scan(&food.EtelID, &food.Nev, &food.Leiras, &food.KepPath, &food.Ar)
 		if err != nil {
 			c.JSON(http.StatusNotFound, gin.H{"error": "Not found"})
 			return
@@ -130,7 +130,7 @@ func PostFood(c *gin.Context) {
 		return
 	}
 
-	_, err := Db.Exec("INSERT INTO Etelek (Nev, Leiras, Kep, Ar) VALUES (?, ?, ?, ?)", newFood.Nev, newFood.Leiras, newFood.Kep, newFood.Ar)
+	_, err := Db.Exec("INSERT INTO Etelek (Nev, Leiras, Kep, Ar) VALUES (?, ?, ?, ?)", newFood.Nev, newFood.Leiras, newFood.KepPath, newFood.Ar)
 	if err != nil {
 		fmt.Println(err)
 		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
@@ -146,7 +146,7 @@ func UpdateFood(c *gin.Context) {
 		return
 	}
 
-	_, err := Db.Exec("UPDATE Etelek SET Nev=?, Leiras=?, Kep=?, Ar=? WHERE EtelID=?", newFood.Nev, newFood.Leiras, newFood.Kep, newFood.Ar, newFood.EtelID)
+	_, err := Db.Exec("UPDATE Etelek SET Nev=?, Leiras=?, Kep=?, Ar=? WHERE EtelID=?", newFood.Nev, newFood.Leiras, newFood.KepPath, newFood.Ar, newFood.EtelID)
 	if err != nil {
 		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
 		return
