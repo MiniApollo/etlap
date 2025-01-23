826fe6492f0346baf4ab40e3bf5d2d8317126f7f
MiniApollo <surmannmark@gmail.com>
2025-01-23 18:14:10 +0100

    Frontend: Add custom brown color to tailwind config

--- website-etlap/tailwind.config.js
+++ website-etlap/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
 		"./src/**/*.{vue,js,ts,jsx,tsx}"
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brown: {
+					DEFAULT: '#e6b999',
+				},
+			}
+		}
 	},
 	plugins: [],
 }
