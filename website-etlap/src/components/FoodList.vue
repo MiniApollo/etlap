17d0e70cd94a902b21caac7ee79390285d1595eb
MiniApollo <surmannmark@gmail.com>
2025-01-23 18:12:31 +0100

    Frontend: Add Style, Grid layout to FoodList

--- website-etlap/src/components/FoodList.vue
+++ website-etlap/src/components/FoodList.vue
@@ -27,22 +27,23 @@ onMounted(() => {
 <template>
     <!-- Fix warnings:
      https://stackoverflow.com/questions/68803137/vue-3-passing-array-warning-extraneous-non-props-attributes-were-passed-to-comp-->
-    <div class="m-auto w-1/2">
-        <h1 class="px-5 text-3xl">Ételek Listája</h1>
+    <div class="m-auto">
+        <h1 class="px-5 text-3xl">Kinálatunk</h1>
         <h2 v-if="foods === undefined || foods.length == 0">
             Hiba történt: <br>
             {{ error }}
         </h2>
-        <ul v-else>
-            <!-- :style="{ 'background-image': 'url(' + food.Kep + ')'}" -->
-            <li v-for="food in foods" class="m-6 h-96 rounded-md border-black border-2 bg-cover bg-no-repeat bg-center"
-                :style="{ 'background-image': 'url(' + 'http://localhost:8080/assets/' + food.KepPath + ')'}">
-                <!-- <img :src="'http://localhost:8080/assets/' + food.KepPath" :alt="food.Nev + ' kép'"> -->
-                <div class="bg-white bg-opacity-90 inline-block rounded-md p-2 max-w-[70%]">
-                    <h3 class="text-2xl">{{ food.Nev }}</h3>
-                    <p>{{ food.Leiras }}</p>
-                    <p>{{ food.Ar }} Ft</p>
-                    <button class="border-2 border-black" @click="$emit('kosarhoz', food)"> Kosárhoz</button>
+        <ul v-else class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+            <li v-for="food in foods" class="m-3 rounded-md border-black border-2 flex flex-col">
+                <img class="object-cover w-full flex-grow basis-2/3 rounded-md"
+                    :src="'http://localhost:8080/assets/' + food.KepPath" :alt="food.Nev + ' kép'">
+                <div class="m-3">
+                    <h3 class="text-4xl font-semibold my-2">{{ food.Nev }}</h3>
+                    <p class="lg:min-h-10">{{ food.Leiras }}</p>
+                    <p class="my-4 text-2xl font-semibold">{{ food.Ar }} Ft</p>
+                    <button
+                        class="p-2 bg-slate-300 font-semibold border-2 rounded-2xl border-black text-black hover:scale-110 transition-all duration-500"
+                        @click="$emit('kosarhoz', food)"> Kosárhoz</button>
                 </div>
             </li>
         </ul>
