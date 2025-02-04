2eaa4bc812df530957e5f12cbb5304c5ce439a87
BudaLevente <72388921+BudaLevente@users.noreply.github.com>
2025-02-04 10:15:18 +0100

    Frontend: Üdvözlő szöveg kidolgozása in welcome

--- website-etlap/src/components/Welcome.vue
+++ website-etlap/src/components/Welcome.vue
@@ -6,23 +6,22 @@
     <div class="grid grid-cols-2 max-md:grid-cols-1">
         <div class="mx-1 lg:mx-16 text-center">
             <h1 class="text-7xl lg:text-8xl my-12">Üdvözöljük étlapunkon</h1>
-            <p class="m-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi architecto beatae
-                expedita
-                nostrum, velit fugit atque sapiente consectetur similique fugiat enim quo dolorem sit? Id quidem tempora
-                asperiores dolore?</p>
+            <p class="m-8">Szolgáltatásunkal rendeld meg ételedet online, hogy rögktön átvehesd amint megérkeztél! 
+                Válassz kedvenc ételeidet közül otthonod kényelméből és mi előre elkészítjük a villámgyors kiszolgálás érdekében.
+            </p>
             <RouterLink
                 class="p-2 bg-slate-300 border-2 rounded-3xl border-black text-black hover:text-xl transition-all duration-500"
                 to="/etelek">Étlap megtekíntése</RouterLink>
         </div>
         <div class="mx-2 my-2 lg:mx-6">
-            <img class="border-4 rounded-md" src="http://localhost:8080/assets/foods/restaurant-3955372_1920.jpg" alt="Rustic food">
+            <img class="border-4 rounded-md" src="http://localhost:8080/assets/foods/singapore.jpg" alt="Rustic food">
         </div>
         <div class="mx-1 lg:mx-16 text-center">
             <h1 class="text-6xl lg:text-7xl mb-8">Megnyitottunk</h1>
-            <p class="m-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi architecto beatae
-                expedita
-                nostrum, velit fugit atque sapiente consectetur similique fugiat enim quo dolorem sit? Id quidem tempora
-                asperiores dolore?</p>
+            <p class="m-8">Hosszas várakozás és fejlesztés után végre megnyitott a várva várt szolgáltatásunk!
+                Célunk egy olyan webes felület létrehozása, amivel vásárlóink eddiginél könnyebben képesek ételt rendelni. 
+
+            </p>
         </div>
         <div class="mx-2 my-2 lg:mx-6">
             <img class="border-4 rounded-md" src="http://localhost:8080/assets/foods/platter-2009590_1920.jpg" alt="Rustic plate">
