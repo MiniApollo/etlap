4d3e16f002c529a3647c7b73d5bd680ec6e4231b
MiniApollo <surmannmark@gmail.com>
2025-02-05 10:23:24 +0100

    Frontend: Format Basket

--- website-etlap/src/components/Basket.vue
+++ website-etlap/src/components/Basket.vue
@@ -46,11 +46,6 @@ function sendOrder() {
     <!-- Fix warnings:
      https://stackoverflow.com/questions/68803137/vue-3-passing-array-warning-extraneous-non-props-attributes-were-passed-to-comp-->
     <div>
-        <h1 class="px-5 text-3xl">Rendelés</h1>
-        <h2 v-if="basketContent === undefined || basketContent.length == 0">
-            A Kosarad Üres
-        </h2>
-        <div v-else class="flex max-sm:flex-col-reverse">
         <h1 class="px-5 text-5xl text-center font-semibold">Rendelés</h1>
         <div class="bg-gray-300 rounded-3xl text-center mx-auto my-9 p-16 w-1/2"
             v-if="basketContent === undefined || basketContent.length == 0">
@@ -59,10 +54,12 @@ function sendOrder() {
                 class="m-2 p-4 font-semibold bg-slate-300 border-2 rounded-3xl border-black text-black hover:text-xl transition-all duration-500"
                 to="/etelek">Rendelj valamit</RouterLink>
         </div>
+        <div v-else class="flex max-sm:flex-col-reverse">
             <ul class="flex flex-col basis-3/5">
-                <li class="m-3 rounded-md border-black border-2 flex max-sm:flex-col" v-for="(food, index) in basketContent">
-                    <img class="w-full basis-1/2 rounded-md"
-                        :src="'http://localhost:8080/assets/foods/' + food.KepPath" :alt="food.Nev + ' kép'">
+                <li class="m-3 rounded-md border-black border-2 flex max-sm:flex-col"
+                    v-for="(food, index) in basketContent">
+                    <img class="w-full basis-1/2 rounded-md" :src="'http://localhost:8080/assets/foods/' + food.KepPath"
+                        :alt="food.Nev + ' kép'">
                     <div class="m-3 basis-1/2">
                         <h3 class="text-4xl font-semibold my-2">{{ food.Nev }}</h3>
                         <p class="lg:min-h-10">{{ food.Leiras }}</p>
@@ -73,15 +70,17 @@ function sendOrder() {
                     </div>
                 </li>
             </ul>
-            <form class="flex flex-col p-16 bg-slate-300 rounded-3xl basis-2/5 md:self-start" @submit.prevent="sendOrder">
+            <form class="flex flex-col p-16 bg-slate-300 rounded-3xl basis-2/5 md:self-start"
+                @submit.prevent="sendOrder">
                 <button
                     class="p-2 bg-red-300 font-semibold border-2 rounded-2xl border-black text-black hover:scale-110 transition-all duration-500 self-center"
                     @click="$emit('emptyBasket')">Kosár kiürítése</button>
 
                 <h1 class="font-semibold text-xl mb-4">Rendelési adatok</h1>
-                
+
                 <label for="Name">Név:</label>
-                <input class="" type="text" id="Name" v-model="newCustomer.Nev" required autocomplete="off" maxlength="128">
+                <input class="" type="text" id="Name" v-model="newCustomer.Nev" required autocomplete="off"
+                    maxlength="128">
 
                 <label for="Email">Email Cím:</label>
                 <input type="email" id="Email" v-model="newCustomer.Email" required autocomplete="off" maxlength="128">
