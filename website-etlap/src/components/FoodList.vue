74bd1baa09befa75230773cb1e2092052ff05ab1
BudaLevente <72388921+BudaLevente@users.noreply.github.com>
2025-02-06 14:03:29 +0100

    Frontend: Style error message

--- website-etlap/src/components/FoodList.vue
+++ website-etlap/src/components/FoodList.vue
@@ -29,10 +29,10 @@ onMounted(() => {
      https://stackoverflow.com/questions/68803137/vue-3-passing-array-warning-extraneous-non-props-attributes-were-passed-to-comp-->
     <div class="m-auto">
         <h1 class="px-5 text-5xl font-semibold">Kinálatunk</h1>
-        <h2 v-if="foods === undefined || foods.length == 0">
-            Hiba történt: <br>
-            {{ error }}
-        </h2>
+        <div class="bg-gray-300 rounded-3xl text-center mx-3 my-9 p-16 md:w-3/4 lg:w-1/2 md:mx-auto" v-if="foods === undefined || foods.length == 0">
+            <h2 class="m-10 text-4xl font-semibold">Hiba történt:</h2>
+            <h2 class="text-2xl">{{ error }}</h2>
+        </div>
         <ul v-else class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             <li v-for="food in foods" class="m-3 rounded-md border-black border-2 flex flex-col">
                 <img class="object-cover w-full flex-grow basis-2/3 rounded-md"
@@ -42,7 +42,7 @@ onMounted(() => {
                     <p class="lg:min-h-10">{{ food.Leiras }}</p>
                     <p class="my-4 text-2xl font-semibold">{{ food.Ar }} Ft</p>
                     <button
-                        class="p-2 bg-slate-300 font-semibold border-2 rounded-2xl border-black text-black hover:scale-110 transition-all duration-500"
+                        class="p-2 bg-slate-300 active:bg-slate-400 font-semibold border-2 rounded-2xl border-black text-black hover:scale-110 transition-all duration-500"
                         @click="$emit('kosarhoz', food)"> Kosárhoz</button>
                 </div>
             </li>
