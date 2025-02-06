c82d4dd69e90883c51029b12f5f045076fc55c70
BudaLevente <72388921+BudaLevente@users.noreply.github.com>
2025-02-06 14:00:58 +0100

    Frontend: Adjust text image sizes in welcome

--- website-etlap/src/components/Welcome.vue
+++ website-etlap/src/components/Welcome.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="grid grid-cols-2 max-md:grid-cols-1">
         <div class="mx-1 lg:mx-16 text-center">
-            <h1 class="text-7xl lg:text-8xl my-12">Üdvözöljük étlapunkon</h1>
+            <h1 class="text-7xl lg:text-8xl my-8">Üdvözöljük étlapunkon</h1>
             <p class="m-8">Szolgáltatásunkal rendeld meg ételedet online, hogy rögktön átvehesd amint megérkeztél! 
                 Válassz kedvenc ételeidet közül otthonod kényelméből és mi előre elkészítjük a villámgyors kiszolgálás érdekében.
             </p>
@@ -13,14 +13,13 @@
                 class="p-2 bg-slate-300 border-2 rounded-3xl border-black text-black hover:text-xl transition-all duration-500"
                 to="/etelek">Étlap megtekíntése</RouterLink>
         </div>
-        <div class="mx-2 my-2 lg:mx-6">
+        <div class="mx-2 my-4 lg:mx-6">
             <img class="border-4 rounded-md" src="http://localhost:8080/assets/foods/singapore.jpg" alt="Rustic food">
         </div>
-        <div class="mx-1 lg:mx-16 text-center">
-            <h1 class="text-6xl lg:text-7xl mb-8">Megnyitottunk</h1>
-            <p class="m-8">Hosszas várakozás és fejlesztés után végre megnyitott a várva várt szolgáltatásunk!
+        <div class="mx-1 lg:mx-16 text-center md:my-6">
+            <h1 class="text-5xl lg:text-7xl mb-8">Megnyitottunk</h1>
+            <p class="m-6">Hosszas várakozás és fejlesztés után végre megnyitott a várva várt szolgáltatásunk!
                 Célunk egy olyan webes felület létrehozása, amivel vásárlóink eddiginél könnyebben képesek ételt rendelni. 
-
             </p>
         </div>
         <div class="mx-2 my-2 lg:mx-6">
