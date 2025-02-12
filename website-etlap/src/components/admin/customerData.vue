3abe88c207b4e8e8d0f0e33a36f1902e628526f5
MiniApollo <surmannmark@gmail.com>
2025-02-12 17:29:30 +0100

    Frontend: Add style to CustomerData Admin

--- website-etlap/src/components/admin/customerData.vue
+++ website-etlap/src/components/admin/customerData.vue
@@ -1,31 +1,39 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 const props = defineProps<{
     customer?: any,
     index?: any,
     foodsByCustomer: any[],
     showCurrentOrders?: boolean
 }>()
+
 </script>
 <template>
     <div>
-        <ul>
-            <li class="inline m-1">{{ customer.Nev }}</li>
-            <li class="inline m-1">{{ customer.VasarloID }}</li>
-            <li class="inline m-1">{{ customer.Email }}</li>
-            <li class="inline m-1">{{ customer.Telefonszam }}</li>
-            <li class="inline m-1">{{ customer.LeadasiIdo }}</li>
-            <button @click="$emit('orderComplete', customer, showCurrentOrders)">
+        <ul class="flex gap-4 max-lg:gap-1 max-lg:flex-col md:justify-between">
+            <li class="basis-1/6 text-xl max-md:text-2xl font-semibold">{{ customer.Nev }}</li>
+            <li class="basis-1/5 text-xl overflow-auto">{{ customer.Email }}</li>
+            <li class="basis-1/5 text-xl overflow-auto">+{{ customer.Telefonszam }}</li>
+            <li class="text-xl overflow-auto">ID: {{ customer.VasarloID }}</li>
+
+            <li class="basis-1/5 text-2xl">{{ customer.LeadasiIdo }}</li>
+            <button
+                class="py-2 px-8 bg-blue-300 hover:bg-blue-400 cursor-pointer font-semibold border-2 rounded-2xl border-black text-black transition-all duration-500 self-start"
+                @click="$emit('orderComplete', customer, showCurrentOrders)">
                 <p v-if="showCurrentOrders">Kész</p>
                 <p v-if="!showCurrentOrders">Visszavon</p>
             </button>
         </ul>
         <ul>
+            <h2 class="mb-3 text-2xl font-semibold">Rendelés:</h2>
             <li v-for="food in foodsByCustomer[index]">
                 <ul>
-                    <li class="inline m-1">{{ food.Nev }}</li>
-                    <li class="inline m-1">{{ food.Ar }}</li>
+                    <li class="my-1 text-2xl inline m-1">{{ food.Nev }}</li>
+                    <li class="my-1 text-2xl inline m-1">{{ food.Ar }} Ft</li>
                 </ul>
             </li>
+            <li class="my-2 text-2xl font-semibold">Összesen: Ft</li>
         </ul>
     </div>
 </template>
