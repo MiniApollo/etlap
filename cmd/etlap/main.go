e130e889592e0cafc88bdb6290d2159f229eef70
MiniApollo <surmannmark@gmail.com>
2025-03-11 18:00:17 +0100

    Backend: Fix 404 error on valid urls

--- cmd/etlap/main.go
+++ cmd/etlap/main.go
@@ -61,8 +61,12 @@ func main() {
 	router.Use(cors.New(config))
 
 	// User API
-	router.StaticFile("/", "./assets/index.html")
 	router.StaticFile("/favicon.ico", "./assets/favicon.ico")
+	routes := []string{"/", "/etelek", "/kosar", "/admin"}
+	for _, url := range routes {
+		router.StaticFile(url, "./assets/index.html")
+	}
+
 	router.Static("/assets", "./assets/")
 
 	router.GET("/food", restapi.GetAllFood)
